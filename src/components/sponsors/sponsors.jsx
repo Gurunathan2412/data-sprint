69411b17562d8e7e4d4ed4262fc7e60c56c8d5d5
Guru <gurunathan2412@gmail.com>
2024-10-13 18:15:30 +0530

    seventeen

--- src/components/sponsors/sponsors.jsx
+++ src/components/sponsors/sponsors.jsx
@@ -23,13 +23,13 @@ const Sponsors=()=>{
                        
         </div>
         <div >
-            { <a target="_blank" href="https://www.idacreations.com/" className="company-btn" data-aos="zoom-in"><img src="https://res.cloudinary.com/dh2mrevv0/image/upload/v1728750100/WhatsApp_Image_2024-10-12_at_20.27.36_dc002bdc_kwecv7.jpg" alt=""/></a> }
+            { <a target="_blank" href="https://www.wewin.co.in/" className="company-btn" data-aos="zoom-in"><img src="https://res.cloudinary.com/dh2mrevv0/image/upload/v1728750100/WhatsApp_Image_2024-10-12_at_20.27.36_dc002bdc_kwecv7.jpg" alt=""/></a> }
             { <div className="spoName">Wewin Industrial Automations</div> }
            
                        
         </div>
         <div >
-            { <a target="_blank" href="https://www.idacreations.com/" className="company-btn" data-aos="zoom-in"><img src="https://res.cloudinary.com/dh2mrevv0/image/upload/v1728750100/WhatsApp_Image_2024-10-12_at_20.27.37_4baa50ed_pof44r.jpg" alt=""/></a> }
+            { <a target="_blank" href="https://www.aghanyatechnology.com/" className="company-btn" data-aos="zoom-in"><img src="https://res.cloudinary.com/dh2mrevv0/image/upload/v1728750100/WhatsApp_Image_2024-10-12_at_20.27.37_4baa50ed_pof44r.jpg" alt=""/></a> }
             { <div className="spoName">Aghanya Technology</div> }
            
                        
